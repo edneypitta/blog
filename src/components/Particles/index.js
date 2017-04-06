76c502b8eb2c3a2e4f0d002caf105a83af5d7b79
Francisco Hisashi Berrocal <eu@chicocode.io>
2017-04-06 19:15:58 -0300

    Fix to get screen width only on client execution

--- src/components/Particles/index.js
+++ src/components/Particles/index.js
@@ -7,9 +7,11 @@ import {particles} from "./index.css"
 
 //this is a linear particles number generator based on screen width
 let linearParticlesGenerator = x => 5/244 * x - 440/61
+const isClient = typeof window !== "undefined"
 
 const ParticlesContainer = () => {
-  config.particles.number.value = linearParticlesGenerator(window.innerWidth)
+  if (isClient)
+    config.particles.number.value = linearParticlesGenerator(window.innerWidth)
 
   return (
     <div className={particles}>
