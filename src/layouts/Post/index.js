77caf3b1d2d3817b53e6c7abf4ac317ed21c10ec
Francisco Hisashi Berrocal <eu@chicocode.io>
2017-04-06 19:17:00 -0300

    Add scrolling to hashed element

--- src/layouts/Post/index.js
+++ src/layouts/Post/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from "react"
+import React, { Component, PropTypes } from "react"
 
 import DisqusThread from "react-disqus-thread"
 
@@ -8,38 +8,54 @@ import Date from "../../components/Date"
 import {getFromContext as get} from "../../i18n/get"
 
 import styles from "./index.css"
+const isClient = typeof window !== "undefined"
 
-const Post = (props, context) => {
-  let i18n = get(context)
-  return (
-      <div>
-          <article className={styles.block}>
-              <h1 className={styles.title}>{ props.head.title }</h1>
-              <Date time={ props.head.date } />
-              <div className={styles.content}>
-                 <Page {...props} />
-              </div>
-          </article>
-          {
-            props.head.comments &&
-            <DisqusThread
-              shortname={i18n.disqus.shortname}
-              identifier={`${context.metadata.pkg.homepage}${props.__url}`}
-              url={`${context.metadata.pkg.homepage}${props.__url}`}
-            />
-          }
-      </div>
-  )
-}
+class Post extends Component {
 
-Post.propTypes = {
-  head: PropTypes.object.isRequired,
-  __url: PropTypes.string
-}
+  static propTypes = {
+    head: PropTypes.object.isRequired,
+    __url: PropTypes.string
+  }
 
-Post.contextTypes = {
-  metadata: PropTypes.object.isRequired,
-  location: PropTypes.object.isRequired
+  static contextTypes = {
+    metadata: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    if(!isClient || !this.context.location.hash)
+      return
+
+    setTimeout(() => {
+       const id = this.context.location.hash.replace('#', '')
+       const element = document.getElementById(id)
+       if (element) element.scrollIntoView()
+     }, 200);
+  }
+
+  render() {
+    const {props, context} = this
+    let i18n = get(context)
+    return (
+        <div>
+            <article className={styles.block}>
+                <h1 className={styles.title}>{ props.head.title }</h1>
+                <Date time={ props.head.date } />
+                <div className={styles.content}>
+                   <Page {...props} />
+                </div>
+            </article>
+            {
+              props.head.comments &&
+              <DisqusThread
+                shortname={i18n.disqus.shortname}
+                identifier={`${context.metadata.pkg.homepage}${props.__url}`}
+                url={`${context.metadata.pkg.homepage}${props.__url}`}
+              />
+            }
+        </div>
+    )
+  }
 }
 
 export default Post
