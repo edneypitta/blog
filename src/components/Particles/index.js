b308b587c83c1823dab8df8d4c57d2443c830d75
Francisco Hisashi Berrocal <eu@chicocode.io>
2017-04-06 18:35:34 -0300

    Adds particles linear function generator

--- src/components/Particles/index.js
+++ src/components/Particles/index.js
@@ -5,7 +5,12 @@ import config from "./config"
 
 import {particles} from "./index.css"
 
+//this is a linear particles number generator based on screen width
+let linearParticlesGenerator = x => 5/244 * x - 440/61
+
 const ParticlesContainer = () => {
+  config.particles.number.value = linearParticlesGenerator(window.innerWidth)
+
   return (
     <div className={particles}>
       <Particles params={config}
